19e6171c38ccb66f461063a1bcc7759b06dbb2de
Preedee Ponchevin <preedee.digital@gmail.com>
2024-03-16 08:48:38 +0700

    - change kafka lib

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::env;
 
-use actix_web::{App, HttpResponse, HttpServer, post, Responder, web};
+use actix_web::middleware::Logger;
+use actix_web::{middleware, post, web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
 use log::{debug, error, info};
-use rdkafka::ClientConfig;
 use rdkafka::error::KafkaError;
 use rdkafka::message::OwnedMessage;
+use rdkafka::ClientConfig;
 use serde_json::Value;
+
+// This struct represents state
+struct AppState {
+    producer: rdkafka::producer::FutureProducer,
+}
 
 #[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
 struct KafkaConfig {
@@ -23,7 +29,6 @@ struct RequestData {
 #[derive(serde::Deserialize, serde::Serialize, Clone, Debug)]
 struct Response {}
 
-
 fn log_produce_result(
     topic: &str,
     result: Result<(i32, i64), (KafkaError, OwnedMessage)>,
@@ -38,13 +43,21 @@ fn log_produce_result(
         })
         .map_err(|(err, _)| error!("kafka error: {}", err))
 }
-async fn post_message_to_kafka(topic_name: &String, message: &Value) {}
+async fn post_message_to_kafka(
+    producer: &rdkafka::producer::FutureProducer,
+    topic_name: &String,
+    message: &Value,
+) {
+}
 
 #[post("/post_message")]
-async fn post_message_handle(req_body: web::Json<RequestData>) -> impl Responder {
+async fn post_message_handle(
+    req_body: web::Json<RequestData>,
+    state: web::Data<AppState>,
+) -> impl Responder {
     debug!("Post message handle : {:#?}", req_body);
 
-    post_message_to_kafka(&req_body.topic_name, &req_body.message).await;
+    post_message_to_kafka(&state.producer, &req_body.topic_name, &req_body.message).await;
 
     let response = Response {};
     HttpResponse::Ok().json(response)
@@ -78,10 +91,19 @@ async fn main() -> std::io::Result<()> {
     config.set("sasl.username", "$ConnectionString");
     config.set("sasl.password", &kafka_config.connection_string);
 
-    let producer: rdkafka::producer::FutureProducer = config.create().expect("Producer creation error");
+    let producer: rdkafka::producer::FutureProducer =
+        config.create().expect("Producer creation error");
+    let app_state = web::Data::new(AppState { producer });
 
-    HttpServer::new(|| App::new().service(post_message_handle))
-        .bind(("0.0.0.0", port))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .wrap(middleware::DefaultHeaders::new().add(("X-Version", "0.2")))
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .app_data(web::Data::new(app_state.clone()))
+            .service(post_message_handle)
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
